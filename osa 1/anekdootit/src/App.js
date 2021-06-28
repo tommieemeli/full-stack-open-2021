2627e2c94768557031e02c80ebf771b5d285110a
Tommi Haapa <tommi-haapa@hotmail.com>
2021-06-28 14:40:28 +0300

    korjaus tehtävään 1.14

--- osa 1/anekdootit/src/App.js
+++ osa 1/anekdootit/src/App.js
@@ -13,7 +13,6 @@ const App = () => {
    
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
-  const [mostVotes, setMost] = useState(0)
 
   const randomAnecdote = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length))
